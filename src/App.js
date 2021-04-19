aef0b1d6c975f87c43c22b34994b7180fd56890e
Varun Khalate <khalatevarun@gmail.com>
2021-04-19 15:26:59 +0530

    Add bottom navigation and routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+import { Container } from '@material-ui/core';
+import React from "react";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
+    <BrowserRouter>
+    <Header/>
+    <div className="app">
+    <Container>
+      <Switch>
+        <Route path= '/'component={Trending} exact/> 
+        <Route path= '/movies'component={Movies}/>
+        <Route path= '/search'component={Search}/>
+        
+      </Switch>
+
+    </Container>
     </div>
+   
+    <SimpleBottomNavigation/>
+    </BrowserRouter>
   );
 }
 

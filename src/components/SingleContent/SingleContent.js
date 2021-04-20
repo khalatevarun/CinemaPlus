66239f5c475c7f4bfe4330480e2b04c9c79938de
Varun Khalate <khalatevarun@gmail.com>
2021-04-21 00:01:32 +0530

    Add badge vote_average to each movie

--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,9 +1,11 @@
 import { img_300, unavailable } from "../../config/config";
+import { Badge } from "@material-ui/core"
 import "./SingleContent.css";
 
 const SingleContent = ({ id, poster, title, date, vote_average }) => {
   return (
     <div className="media">
+      <Badge badgeContent={vote_average} color={vote_average>6? 'primary':'secondary'}/>
       <img
         className="poster"
         alt={title}

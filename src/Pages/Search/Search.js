d48ef270c8371f0b1968251b159ced6a84cc57f6
Varun Khalate <khalatevarun@gmail.com>
2021-04-23 17:37:38 +0530

    Design Search Page and add Model component

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,103 @@
+import {
+  Button,
+  createMuiTheme,
+  Tab,
+  Tabs,
+  TextField,
+  ThemeProvider,
+} from "@material-ui/core";
+import { useState, useEffect } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import axios from "axios";
+import SingleContent from "../../components/SingleContent/SingleContent";
+import CustomPagination from "../../components/Pagination/CustomPagination";
+
 const Search = () => {
-    return (
-        <div>
-            <span className='pageTitle'>Search</span>
+  const [type, setType] = useState(0);
+  const [page, setPage] = useState(1);
+  const [searchText, setSearchText] = useState("");
+  const [content, setContent] = useState();
+  const [numOfPages, setNumOfPages] = useState();
+
+  const darkTheme = createMuiTheme({
+    palette: {
+      type: "dark",
+      primary: {
+        main: "#fff",
+      },
+    },
+  });
+
+  const fetchSearch = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/search/${type ? "tv" : "movie"}?api_key=${
+        process.env.REACT_APP_API_KEY
+      }&language=en-US&query=${searchText}&page=${page}&include_adult=false`
+    );
+
+    setContent(data.results);
+    setNumOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchSearch();
+    //eslint-disable-next-line
+  }, [type, page]);
+
+  return (
+    <div>
+      <ThemeProvider theme={darkTheme}>
+        <div style={{ display: "flex", paddingTop: "15px" }}>
+          <TextField
+            style={{ flex: 1 }}
+            className="searchBox"
+            label="search"
+            variant="filled"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+          <Button variant="contained" style={{ marginLeft: 10 }} onClick={fetchSearch}>
+            <SearchIcon />
+          </Button>
         </div>
-    )
-}
+
+        <Tabs
+          value={type}
+          indicatorColor="primary"
+          textColor="primary"
+          onChange={(event, newValue) => {
+            setType(newValue);
+            setPage(1);
+          }}
+          style={{ paddingBottom: 5, paddingTop:5, marginTop:5 }}
+        >
+          <Tab style={{ width: "50%" }} label="Search Movies" />
+          <Tab style={{ width: "50%" }} label="Search TV Series" />
+        </Tabs>
+      </ThemeProvider>
+      <div>
+     
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <SingleContent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.release_date || c.first_air_date}
+              media_type={type? "tv":"movies"}
+              vote_average={c.vote_average}
+            />
+          ))}
+          {searchText && !content && (type ? <h2>No Tv Series Found</h2>: <h2>No Movies Found</h2>)}
+      </div>
+      {numOfPages >1 && (
+      <CustomPagination setPage={setPage} />
+      )}
+    </div>
+    </div>
+  );
+};
 
 export default Search;
--- src/components/SingleContent/SingleContent.js
+++ src/components/SingleContent/SingleContent.js
@@ -1,10 +1,11 @@
 import { img_300, unavailable } from "../../config/config";
 import { Badge } from "@material-ui/core"
 import "./SingleContent.css";
+import ContentModal from "../ContentModal/ContentModal";
 
 const SingleContent = ({ id, poster, title, date, media_type, vote_average }) => {
   return (
-    <div className="media">
+    <ContentModal media_type={media_type} id={id} >
       <Badge badgeContent={vote_average} color={vote_average>6? 'primary':'secondary'}/>
       <img
         className="poster"
@@ -17,7 +18,7 @@ const SingleContent = ({ id, poster, title, date, media_type, vote_average }) =>
         <p className="date">{date}</p>
       </div>
      
-    </div>
+    </ContentModal>
   );
 };
 
